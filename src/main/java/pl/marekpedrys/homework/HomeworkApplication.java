daf12a41deb5c12bbc64cef48edbf52c83237045
Marek Pedrys <marek.pedrys@gmail.com>
2024-07-14 00:15:21 +0200

    PostgreSQL configuration added

--- src/main/java/pl/marekpedrys/homework/HomeworkApplication.java
+++ src/main/java/pl/marekpedrys/homework/HomeworkApplication.java
@@ -2,8 +2,10 @@ package pl.marekpedrys.homework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:db-postgres.properties")
 public class HomeworkApplication {
 
 	public static void main(String[] args) {
