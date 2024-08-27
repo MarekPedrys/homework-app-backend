d5c0a75b9ed7c0f32e747c7f119d1104659c6766
Marek Pedrys <marek.pedrys@gmail.com>
2024-08-27 21:40:33 +0200

    Initial commit

---
+++ src/main/java/pl/marekpedrys/mathematicalproblems/MathematicalProblemsApplication.java
@@ -0,0 +1,13 @@
+package pl.marekpedrys.mathematicalproblems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathematicalProblemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathematicalProblemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/marekpedrys/mathematicalproblems/MathematicalProblemsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.marekpedrys.mathematicalproblems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathematicalProblemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
