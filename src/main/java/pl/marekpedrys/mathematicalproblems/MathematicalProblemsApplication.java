46fdf4a32bf0f9eccd679a4ecbea3452616bac7f
Marek Pedrys <marek.pedrys@gmail.com>
2024-08-27 22:04:12 +0200

    added PostgreSQL configuration

--- src/main/java/pl/marekpedrys/mathematicalproblems/MathematicalProblemsApplication.java
+++ src/main/java/pl/marekpedrys/mathematicalproblems/MathematicalProblemsApplication.java
@@ -2,8 +2,10 @@ package pl.marekpedrys.mathematicalproblems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:db-postgres.properties")
 public class MathematicalProblemsApplication {
 
 	public static void main(String[] args) {
